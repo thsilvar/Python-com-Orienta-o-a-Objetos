3474cbc34a66682d841d91855cd91464a5159aea
Manu Correa <thsilvar@gmail.com>
2025-01-26 21:39:32 -0300

    boas práticas

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ print('Sabor Express \n')
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
-print('4. Sair')
+print('4. Sair \n')
+
+opcao_escolhida = input('Digite a opção desejada: ')
+
+print('Opção escolhida: ', opcao_escolhida)
+
