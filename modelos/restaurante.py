bf5b0dd0b40cf67063dd5243485e40a951a7cace
Manu Correa <thsilvar@gmail.com>
2025-02-19 13:32:54 -0300

    metodos

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,13 +1,21 @@
 class Restautante:
+    restaurantes = []
+
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restautante.restaurantes.append(self)
+
+    def __str__(self):
+        return f'{self.nome} ({self.categoria})'
+
+    def listar_restaurantes():
+        for restaurante in Restautante.restaurantes:
+            print(f'{restaurante.nome} ({restaurante.categoria}) - {restaurante.ativo}')    
 
 restaurante_praca = Restautante('Restaurante da Praça', 'Comida Brasileira')
 restaurante_pizza = Restautante('Pizzaria do Bairro', 'Pizzaria')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+Restautante.listar_restaurantes()
 
-print(vars(restaurante_praca))
-print(vars(restaurante_pizza))
