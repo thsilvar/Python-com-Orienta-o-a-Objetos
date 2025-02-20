15bf9b4f76a00c6ac2ec371627bf52242be4c50f
Manu Correa <thsilvar@gmail.com>
2025-02-20 18:37:36 -0300

    metodos

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,24 +2,29 @@ class Restautante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restautante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} ({self.categoria})'
+        return f'{self._nome} ({self._categoria})'
 
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Ativo')
-        for restaurante in Restautante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')    
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')    
 
     @property
     def ativo(self):
-        return  'Sim' if self._ativo else 'Não'
+        return '⌧' if self._ativo else '☐'
+    
+    def alternar_status(self):
+        self._ativo = not self._ativo
 
-restaurante_praca = Restautante('Restaurante da Praça', 'Comida Brasileira')
+restaurante_praca = Restautante('restaurante da Praça', 'Comida Brasileira')
+restaurante_praca.alternar_status()
 restaurante_pizza = Restautante('Pizzaria do Bairro', 'Pizzaria')
 
 Restautante.listar_restaurantes()
