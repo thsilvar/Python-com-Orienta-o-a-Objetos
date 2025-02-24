ef9f65163f6374ed3e9e4223fd417ad41a8f4318
Manu Correa <thsilvar@gmail.com>
2025-02-24 19:09:26 -0300

    Herança

---
+++ modelos/cardapio/bebida.py
@@ -0,0 +1,6 @@
+import modelos.cardapio.item_cardapio as ItemCardapio
+
+class Bebida(ItemCardapio):
+    def __init__(self, nome, preco, tamanho):
+        super().__init__(nome, preco)
+        self._tamanho = tamanho
---
+++ modelos/cardapio/prato.py
@@ -0,0 +1,6 @@
+import modelos.cardapio.item_cardapio as ItemCardapio
+
+class Prato(ItemCardapio):
+    def __init__(self, nome, preco, descricao):
+        super().__init__(nome, preco)
+        self._descricao = descricao
