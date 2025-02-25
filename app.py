2b2a96622b32d887d3a6cf7bf96d6f93dc6abadd
Manu Correa <thsilvar@gmail.com>
2025-02-25 13:36:13 -0300

    criando arquivos com python

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
 
@@ -20,4 +21,8 @@ if response.status_code == 200:
 else:
     print('Erro ao acessar a API')
 
-print(dados_restaurante['McDonald’s'])
+for nome_do_restaurante, dados in dados_restaurante.items():
+   nome_do_arquivo = f'{nome_do_restaurante}.json'
+   with open(nome_do_arquivo,'w') as arquivo_restaurante:
+        json.dump(dados, arquivo_restaurante, indent=4)
+   
