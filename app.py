654247362571d9b03fd1fa9fbb4ad27143767017
Manu Correa <thsilvar@gmail.com>
2025-02-25 13:25:42 -0300

    Filtrando dados

--- app.py
+++ app.py
@@ -4,4 +4,20 @@ url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
 
 response = requests.get(url)
 if response.status_code == 200:
-    print(response.json())
+    dados_json = response.json()
+    dados_restaurante = {}
+
+    for item in dados_json:
+        nome_do_restaurante = item['Company']
+        if nome_do_restaurante not in dados_restaurante:
+            dados_restaurante[nome_do_restaurante] = []
+        
+        dados_restaurante[nome_do_restaurante].append({
+            "item: ": item['Item'],
+            "price: ": item['price'],
+            "description: ": item['description']
+        })
+else:
+    print('Erro ao acessar a API')
+
+print(dados_restaurante['McDonald’s'])
