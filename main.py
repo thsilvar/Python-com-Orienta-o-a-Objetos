3ca66f898d2ecc47c88c3bbf0fd9dfc1eaf80e0c
Manu Correa <thsilvar@gmail.com>
2025-02-25 22:37:26 -0300

    Criando um endpoint

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+import requests
 
 app = FastAPI()
+
 
 @app.get('/api/hello')
 def read_root():
     return {'Hello': 'World'}
+
+
+@app.get('/api/restaurantes/')
+def get_restaurantes(restaurante: str = Query(None)):
+    url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        dados_json = response.json()
+        if restaurante is None:
+            return {'Dados':dados_json}
+        
+        dados_restaurante = []
+        for item in dados_json:
+            if item['Company'] == restaurante:
+                dados_restaurante.append({
+                    "item: ": item['Item'],
+                    "price: ": item['price'],
+                    "description: ": item['description']
+                })
+        return {'Restaurante':restaurante, 'Cardapio':dados_restaurante}
+    else:
+        return {'Error': f'{response.status_code}'}
